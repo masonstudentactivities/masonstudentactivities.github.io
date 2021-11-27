dcc7df49be2f8f98b0be56da1f148df40ec66304
Luke Trenaman <trenamanluke@gmail.com>
2021-11-26 22:53:32 -0500

    Begin adding React to site

---
+++ buildsite.js
@@ -0,0 +1,81 @@
+"use strict";
+
+function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return !!right[Symbol.hasInstance](left); } else { return left instanceof right; } }
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+//To convert this into buildsite.js, use the babel transpiler found below:
+//https://babeljs.io/en/repl#?browsers=&build=&builtIns=false&corejs=3.6&spec=false&loose=false&code_lz=DwIwrgLhD2B2AEcDCAbAlgYwNYF4DeAFAJTw4B88EAFmgM4B0tAphAMoQCGETBe86WJgBMAXJQBOYJvAC-RGWQBQ8FfAAyaQYuAB6cFDhkgA&debug=false&forceAllTransforms=false&shippedProposals=false&circleciRepo=&evaluate=false&fileSize=false&timeTravel=false&sourceType=module&lineWrap=true&presets=es2015%2Creact%2Cstage-2&prettier=false&targets=&version=7.15.7&externalPlugins=&assumptions=%7B%7D
+var request = new XMLHttpRequest();
+request.open('GET', '/pages.json');
+request.send();
+
+request.onload = function () {
+  var response = JSON.parse(request.responseText);
+  console.log(response);
+  var cards = [];
+
+  function transformURL(url) {
+    var id = url.split("https://drive.google.com/open?id=")[1];
+    return "https://drive.google.com/uc?export=view&id=" + id;
+  }
+
+  for (var i = 0; i < response.length; i++) {
+    cards.push( /*#__PURE__*/React.createElement("a", {
+      className: "thumbnail col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2",
+      key: i,
+      href: "?" + response[i].clubName
+    }, /*#__PURE__*/React.createElement("img", {
+      src: transformURL(response[i].thumbURL)
+    }), /*#__PURE__*/React.createElement("h2", null, response[i].clubName)));
+  }
+
+  console.log(cards);
+
+  var App = /*#__PURE__*/function (_React$Component) {
+    _inherits(App, _React$Component);
+
+    var _super = _createSuper(App);
+
+    function App() {
+      _classCallCheck(this, App);
+
+      return _super.apply(this, arguments);
+    }
+
+    _createClass(App, [{
+      key: "render",
+      value: function render() {
+        return /*#__PURE__*/React.createElement("div", {
+          className: "container"
+        }, /*#__PURE__*/React.createElement("div", {
+          className: "row"
+        }, cards));
+      }
+    }]);
+
+    return App;
+  }(React.Component);
+
+  ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById('root'));
+};
---
+++ buildsite.jsx
@@ -0,0 +1,39 @@
+//To convert this into buildsite.js, use the babel transpiler found below:
+//https://babeljs.io/en/repl#?browsers=&build=&builtIns=false&corejs=3.6&spec=false&loose=false&code_lz=DwIwrgLhD2B2AEcDCAbAlgYwNYF4DeAFAJTw4B88EAFmgM4B0tAphAMoQCGETBe86WJgBMAXJQBOYJvAC-RGWQBQ8FfAAyaQYuAB6cFDhkgA&debug=false&forceAllTransforms=false&shippedProposals=false&circleciRepo=&evaluate=false&fileSize=false&timeTravel=false&sourceType=module&lineWrap=true&presets=es2015%2Creact%2Cstage-2&prettier=false&targets=&version=7.15.7&externalPlugins=&assumptions=%7B%7D
+var request = new XMLHttpRequest();
+request.open('GET', '/pages.json');
+request.send();
+request.onload = function(){
+    let response = JSON.parse(request.responseText);
+    console.log(response);
+    let cards = [];
+
+    function transformURL(url){
+      let id = url.split("https://drive.google.com/open?id=")[1];
+      return "https://drive.google.com/uc?export=view&id=" + id;
+    }
+    for(let i = 0;i<response.length;i++){
+        cards.push(
+          <a className="thumbnail col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2" key={i} href={"?" + response[i].clubName}>
+            <img src={transformURL(response[i].thumbURL)}/>
+            <h2>{response[i].clubName}</h2>
+          </a>
+        )
+    }
+    console.log(cards);
+    class App extends React.Component {
+        render() {
+          return (
+            <div className="container">
+              <div className="row">
+                {cards}
+              </div>
+            </div>
+          );
+        }
+      }
+    ReactDOM.render(
+        <App/>,
+        document.getElementById('root')
+      );
+};
