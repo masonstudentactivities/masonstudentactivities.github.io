3e9508f63264f6b047644c85cdd2412b83a01b82
Luke Trenaman <trenamanluke@gmail.com>
2021-12-20 13:53:38 -0500

    Comment the JSX code

--- buildsite.jsx
+++ buildsite.jsx
@@ -1,5 +1,7 @@
 //To convert this into buildsite.js, use the babel transpiler found below:
 //https://babeljs.io/en/repl#?browsers=&build=&builtIns=false&corejs=3.6&spec=false&loose=false&code_lz=DwIwrgLhD2B2AEcDCAbAlgYwNYF4DeAFAJTw4B88EAFmgM4B0tAphAMoQCGETBe86WJgBMAXJQBOYJvAC-RGWQBQ8FfAAyaQYuAB6cFDhkgA&debug=false&forceAllTransforms=false&shippedProposals=false&circleciRepo=&evaluate=false&fileSize=false&timeTravel=false&sourceType=module&lineWrap=true&presets=es2015%2Creact%2Cstage-2&prettier=false&targets=&version=7.15.7&externalPlugins=&assumptions=%7B%7D
+
+//Get the JSON contained within our git repo
 var request = new XMLHttpRequest();
 request.open('GET', '/pages.json');
 request.send();
@@ -8,12 +10,16 @@ request.onload = function(){
     console.log(response);
     let cards = [];
 
-    
+    //Turn our current drive link into one that displays image previews
     function transformURL(url){
       let id = url.split("https://drive.google.com/open?id=")[1];
       return "https://drive.google.com/uc?export=view&id=" + id;
     }
+    //Are we on a subpage? Check by using URL variables
+    //Subpages woudld look like: masonstudentactivities.github.io?chess
+    //argName would look like: chess
     let argName = decodeURI(location.search.split("?")[1]);
+    //We extracted argName, but is it a valid club?
     let subPage = false;
     let clubData = {};
     for(let i = 0;i< response.length;i++){
@@ -22,6 +28,7 @@ request.onload = function(){
         clubData = response[i];
       }
     }
+    //If URL variables don't show a valid club, build an array of thumbnail cards
     if(!subPage){
       for(let i = 0;i<response.length;i++){
           cards.push(
@@ -36,6 +43,7 @@ request.onload = function(){
     console.log(cards);
     class App extends React.Component {
         render() {
+          //If we aren't on a valid club page, render the thumbnails page
           if(!subPage){
             return (
               <div className="container">
@@ -45,6 +53,7 @@ request.onload = function(){
               </div>
             );
           }
+          //If we are on a valid club page, render the club using clubData
           if(subPage){
             return(
               <div className="container">
