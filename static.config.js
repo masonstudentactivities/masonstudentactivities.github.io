106c7766f65fbddb7265b58903ca6b5f80dfa8f5
Luke Trenaman <trenamanluke@gmail.com>
2021-12-30 01:03:49 -0500

    Modify static.config.js

--- static.config.js
+++ static.config.js
@@ -10,24 +10,24 @@ export default {
   siteRoot: "https://masonstudentactivities.github.io",
   getRoutes: async () => {
     const pages = require("./src/pages.json");
+    let pagesObj = pages.map(page => ({
+        path: `/${page.name}`,
+        template: 'src/Page',
+        getData: () => ({
+          "data":page
+        })
+    }))
+    console.log(pagesObj);
     return [
       {
         path: '/',
-        getData: () => ({
-          "data":pages
-        }),
-        children: pages.map(page => ({
-          path: `/${page.name}`,
-          template: 'src/Page',
-          getData: () => ({
-            "data":page
-          }),
-        })),
+        template: 'src/templates/index'
       },
+      ...pagesObj
     ]
   },
   plugins:[
-    require.resolve('react-static-plugin-reach-router')
-  ]
+    require.resolve('react-static-plugin-reach-router'),
+    require.resolve('react-static-plugin-sitemap'),]
   // Remove this when you start doing any static generation
 }
