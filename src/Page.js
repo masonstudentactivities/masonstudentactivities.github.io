d28d43519c154ae5ba0aebb22e84e9517258034b
Luke Trenaman <trenamanluke@gmail.com>
2021-12-31 14:33:47 -0500

    Add methods handling optional club info

--- src/Page.js
+++ src/Page.js
@@ -15,28 +15,56 @@ function Page() {
                 <Link to={"/?filter=" + data.category}>{data.category}</Link> &gt; 
                 <Link to={"/" + data.name}>{data.name}</Link> </p>
             <h2>{data.name}</h2>
-            <a href={data.schoologyPage}>Schoology Link</a>&nbsp;
-            <a href={data.website}>Website Link</a>
+            {
+                data.schoologyPage !== undefined &&
+                <a href={data.schoologyPage} className="site-link">Schoology Link</a>
+            }
+            {
+                data.website !== undefined &&
+                <a href={data.website} className="site-link">Website Link</a>
+            }
             <hr/>
             <h4>About this club</h4>
             <p>{data.description}</p>
-            <h4>{data.sectionOneTitle}</h4> 
-            <p>{data.sectionOneContent}</p>
-            <h4>{data.sectionTwoTitle}</h4>
-            <p>{data.sectionTwoContent}</p>
-            <h4>{data.sectionThreeTitle}</h4>
-            <p>{data.sectionThreeContent}</p>
+            {
+                data.sectionOneTitle !== undefined &&
+                <>
+                <h4>{data.sectionOneTitle}</h4> 
+                <p>{data.sectionOneContent}</p>
+                </>
+            }
+            {
+                data.sectionTwoTitle !== undefined &&
+                <>
+                <h4>{data.sectionTwoTitle}</h4> 
+                <p>{data.sectionTwoContent}</p>
+                </>
+            }
+            {
+                data.sectionThreeTitle !== undefined &&
+                <>
+                <h4>{data.sectionThreeTitle}</h4> 
+                <p>{data.sectionThreeContent}</p>
+                </>
+            }
 
             <h4>Contact Information</h4>
             <p>Contact anyone below for more information about the club:</p>
             <p>Advisor: {data.advisorName} @ {data.advisorEmail}</p>
-            <p>{data.additionalTitle}: {data.additionalName} @ {data.additionalEmail}</p>
+            {data.additionalTitle !== undefined &&
+                <p>{data.additionalTitle}: {data.additionalName} @ {data.additionalEmail}</p>
+            }
 
             <h4>Accesibility information</h4>
             <p>Mobility Rating: {data.mobilityRating}</p>
             <p>Sound Rating: {data.soundRating}</p>
-            <h4>Accomodations</h4>
-            <p>{data.sectionAccomodationsContent}</p>
+            {
+                data.sectionAccomodationsContent !== undefined &&
+                <>
+                    <h4>Accomodations</h4>
+                    <p>{data.sectionAccomodationsContent}</p>
+                </>
+            }
             <p>Updated on: {formatDate(data.timestamp)}</p>
 
 
