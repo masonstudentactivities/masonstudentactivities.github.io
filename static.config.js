082e828096c78e86090bfaa814de5fc967c85683
Luke Trenaman <trenamanluke@gmail.com>
2022-01-04 14:18:13 -0500

    Reference manifest and logos at root directory

--- static.config.js
+++ static.config.js
@@ -23,8 +23,8 @@ export default {
           name="description"
           content="Web site created using create-react-app"
         />
-        <link rel="apple-touch-icon" href="logo192.png" />
-        <link rel="manifest" href="manifest.json" />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        <link rel="manifest" href="/manifest.json" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <title>React App</title>
