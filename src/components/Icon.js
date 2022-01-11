1aebb09a77f8370acc6268be8be4363ee8dc56a2
Jessica Li <a38814203@outlook.com>
2022-01-11 15:40:34 -0500

    icons

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -3,7 +3,7 @@ function Icon(props){
     return(
         // props.type: mobility
         // props.level: Low
-        <img src={"/icons/" + props.level + "_" + props.type.toLowerCase() + ".png"} 
+        <img className="icon" src={"/icons/" + props.level + "_" + props.type.toLowerCase() + ".png"} 
             data-bs-placement="top" data-bs-toggle="tooltip" title={props.level + " " + props.type}/>
     );
 }
