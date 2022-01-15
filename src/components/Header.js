868d76f752a3bf24d83ca5631683d33b139e90b4
Luke Trenaman <trenamanluke@gmail.com>
2022-01-14 22:45:04 -0500

    Multipage now reflects different titles for pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useRouteData } from 'react-static';
 function Header(props){
+    let { site } = useRouteData();
     return(
         <header>
             <div className="container">
-                <h1>MHS Student Activities</h1>
+                <h1>{site.directory.toUpperCase()} Student Activities</h1>
                 <p>This site is currently under development! To find student activities information please visit the <a href = "https://sites.google.com/masonohioschools.com/mhsactivities2021/home?scrlybrkr=e3f4ff9b">Google Site</a></p>
             </div>
             { props.navAddition !== undefined && props.navAddition }
