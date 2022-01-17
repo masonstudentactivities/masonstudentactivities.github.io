8e4c2072dcd8eb95adbe1cf30dacb36e4606c36f
Luke Trenaman <trenamanluke@gmail.com>
2022-01-17 17:44:44 -0500

    New thumbnail folder structure

--- src/content/homepage/Thumbnail.js
+++ src/content/homepage/Thumbnail.js
@@ -9,7 +9,7 @@ function Thumbnail(props){
     let {site} = useRouteData();
     return(
         <Link to={`/${site.directory}/${page.name}`} className={"thumbnail col-6 col-sm-4 col-md-3 col-lg-2 col-xl-2 " + (props.shouldBeVisible ? "" : "hidden")}>
-        <img src={"/images/thumbnails/" +page.name + "." + page.fileExtension}/>
+        <img src={`/images/thumbnails/${site.directory}/${page.name}.${page.fileExtension}`}/>
         <h2>{page.name}</h2>
         <Icon type="sound" level={page.soundRating}></Icon>
         <Icon type="mobility" level={page.mobilityRating}></Icon>
