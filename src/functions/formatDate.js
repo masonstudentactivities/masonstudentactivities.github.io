e76b51a82a45bdf09f03166b4ff84d21a58b461d
Luke Trenaman <trenamanluke@gmail.com>
2022-02-24 16:37:47 -0500

    Change getDay to getDate

--- src/functions/formatDate.js
+++ src/functions/formatDate.js
@@ -2,7 +2,7 @@ function formatDate(dateString){
     let timestamp = new Date(dateString);
     let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
     let month = months[timestamp.getMonth()];
-    let day = timestamp.getDay();
+    let day = timestamp.getDate();
     let year = timestamp.getFullYear();
     return `${month} ${day}, ${year}`
 
