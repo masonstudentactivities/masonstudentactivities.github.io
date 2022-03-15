3de74d156f977e6fb15d1101eef0a16574b63c66
Luke Trenaman <trenamanluke@gmail.com>
2022-03-15 15:20:00 -0400

    Calendar hatred

--- src/content/calendar/Calendar.js
+++ src/content/calendar/Calendar.js
@@ -4,18 +4,36 @@ import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
 
 export default class DemoApp extends React.Component {
+  addOneDay(dateString){
+    var startDate = new Date(dateString);
+    console.log(startDate);
+    startDate.setDate(startDate.getDate() + 1)
 
+    // endDate.setDate(startDate.getDate() + 1);
+     var newString = startDate.toLocaleDateString("en-GB").split("/");
+     return newString[2] + "-0" + newString[0] + "-0" + newString[1];
+  }
   render() {
     return (
       <FullCalendar
         plugins={[ dayGridPlugin, interactionPlugin ]}
-        dateClick={this.handleDateClick}
+        eventClick={this.handleEventClick}
+        events={[
+            { title: 'event 1', date: '2022-03-01' },
+            { title: 'event 1', date: '2022-03-02' },
+            { title: 'event 2', date: '2022-03-02' },
+            {
+                title:"Multiple days",
+                start:'2022-03-02',
+                end:this.addOneDay('2022-03-05 UTC-5')
+            }
+          ]}
       />
     )
   }
 
-  handleDateClick = (arg) => { // bind with an arrow function
-    alert(arg.dateStr)
+  handleEventClick = (info) => { // bind with an arrow function
+    alert(info.event.title)
   }
 
 }
