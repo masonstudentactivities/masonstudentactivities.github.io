571f0d1d67d35723c0ce38d9af31c853455ca10a
Luke Trenaman <trenamanluke@gmail.com>
2022-03-22 18:37:11 -0400

    Forgot to encapsulate calendar in a window check

--- src/content/calendar/Calendar.js
+++ src/content/calendar/Calendar.js
@@ -6,28 +6,34 @@ import googleCalendarPlugin from "@fullcalendar/google-calendar"
 import events from "../../eventsMHS.json";
 export default class Calendar extends React.Component {
   render() {
-    return (
-      <>
-      <FullCalendar
-        plugins={[interaction, dayGridPlugin,googleCalendarPlugin ]}
-        googleCalendarApiKey= 'AIzaSyC9Z83mzJ3NX6c21TPhqy0fF9oVvz2akBo'
-        events= {
-          {googleCalendarId: 'c_mf656a42e6fjpv1sb1h5c75v1o@group.calendar.google.com'}
-        }
+    if (typeof window !== "undefined") {
+      return (
+        <>
+        <FullCalendar
+          plugins={[interaction, dayGridPlugin,googleCalendarPlugin ]}
+          googleCalendarApiKey= 'AIzaSyC9Z83mzJ3NX6c21TPhqy0fF9oVvz2akBo'
+          events= {
+            {googleCalendarId: 'c_mf656a42e6fjpv1sb1h5c75v1o@group.calendar.google.com'}
+          }
 
-        dayMaxEventRows={3}
-        views={
-          {
-            dayGridMonth: {
-              dayMaxEventRows: 3 // adjust to 6 only for timeGridWeek/timeGridDay
+          dayMaxEventRows={3}
+          views={
+            {
+              dayGridMonth: {
+                dayMaxEventRows: 3 // adjust to 6 only for timeGridWeek/timeGridDay
+              }
             }
           }
-        }
-        initialView={this.props.view}
-        height={this.props.view === "dayGridWeek" ? 200 : undefined}
-      />
-      </>
-    )
-  }
+          initialView={this.props.view}
+          height={this.props.view === "dayGridWeek" ? 200 : undefined}
+        />
+        </>
+      )
+    } else{
+      return(
+        <p>This calendar requires JavaScript to run.</p>
+      )
+    }
 
+  }
 }
